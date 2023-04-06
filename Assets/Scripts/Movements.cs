85f76eb848a24c270c1ff81f84cc242c152fda8c
KhenanDpy <khdupuy@gmail.com>
2023-04-06 23:52:01 +0200

    Enfin une idée !
    Idée de faire un ciel où on saute de plateforme en plateforme (en forme de nuage) pour arriver dans un genre d'arène où on affronte des monstres.
    (En tout cas c'est un début d'idée)

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -6,7 +6,18 @@ using UnityEngine.UIElements;
 public class Movements : MonoBehaviour
 {
     [SerializeField]
+    CharacterController charCC;
+
+    [SerializeField]
     Rigidbody cubeRb;
+
+    [SerializeField]
+    float movementSpeed = 0.05f, gravity;
+
+    Vector3 movementDirection = Vector3.zero;
+
+    [SerializeField]
+    Camera cameraGO; // permet de bouger le personnage par rapport à la camera
 
     // Start is called before the first frame update
     void Start()
@@ -17,25 +28,32 @@ public class Movements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movementDirection.y -= gravity * Time.deltaTime;
+        Debug.Log(movementDirection.y);
         if (Input.GetKey(KeyCode.Z))
         {
-            cubeRb.AddForce(transform.forward * 6f, ForceMode.Force);
+            charCC.Move(cameraGO.transform.forward * movementSpeed);
+            cubeRb.AddForce(cameraGO.transform.forward * 6f, ForceMode.Force);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            cubeRb.AddForce(transform.forward * -6f, ForceMode.Force);
+            charCC.Move(cameraGO.transform.forward * -movementSpeed);
+            cubeRb.AddForce(cameraGO.transform.forward * -6f, ForceMode.Force);
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            cubeRb.AddForce(transform.right * -6f, ForceMode.Force);
+            charCC.Move(cameraGO.transform.right * -movementSpeed);
+            cubeRb.AddForce(cameraGO.transform.right * -6f, ForceMode.Force);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            cubeRb.AddForce(transform.right * 6f, ForceMode.Force);
+            charCC.Move(cameraGO.transform.right * movementSpeed);
+            cubeRb.AddForce(cameraGO.transform.right * 6f, ForceMode.Force);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            cubeRb.AddForce(transform.up * 600f, ForceMode.Force);
+            charCC.Move(cameraGO.transform.up * movementSpeed * 50);
+            cubeRb.AddForce(cameraGO.transform.up * 300f, ForceMode.Force);
         }
     }
 }
