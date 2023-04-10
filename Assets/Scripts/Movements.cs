05e79a907520fb836c4e0d228eedf692083a8069
KhenanDpy <khdupuy@gmail.com>
2023-04-10 19:56:44 +0200

    Pleins d'ajustements
    sauter sur les plateformes
    des ajustements dans le code,
    petits soucis de dérapage,
    bidouillages sur les animations
    (le saut est bizarre)

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -9,72 +9,102 @@ public class Movements : MonoBehaviour
     Rigidbody rb;
 
     [SerializeField]
-    float rotate;
+    Text text;
 
     [SerializeField]
     List<AnimatorController> animations = new List<AnimatorController>();
 
-    private Vector3 oldMousePosition;
+    [SerializeField]
+    float speed, jump, limit;
 
-    Vector3 m_EulerAngleVelocity;
+    float rotationY = 0f;
+    public float sensitivity = 15f;
+
+    //Grounded
+    bool isGrounded;
+    float groundedOffset = -0.14f;
+    float groundedRadius = 0.28f;
+    public LayerMask groundLayers;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        oldMousePosition = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
-        if ((Input.anyKey == false) && (rb.position.y<=0.1f))
+        // rotation par la souris
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+        rb.transform.localEulerAngles = new Vector3(0, rotationY, 0);
+
+        animator.SetBool("Forward", false);
+
+        // si on est au sol, on peut se déplacer
+        if (rb.position.y <= 0.02)
         {
-            animator.runtimeAnimatorController = animations.ToArray()[1];
-            //rb.velocity = Vector3.zero;
+            animator.SetBool("Jump", false);
+
+            // Positionnement de la sphère au pied de l'Armature
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
+            // On teste les collisions de la sphère dans le GroundedLayers
+            isGrounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
+            animator.SetBool("Grounded", isGrounded);
+
+            if ((Input.anyKey == false))//&& (rb.position.y<=0.1f))
+            {
+                //animator.runtimeAnimatorController = animations.ToArray()[1];
+                //rb.velocity = Vector3.zero; //stop les glissades
+                animator.SetBool("Idle", true);
+                rb.drag = 5f; // pour pas glisser
+            }
+            else
+            {
+                animator.SetBool("Idle", false);
+                rb.drag = 0f;
+            }
+
+            if (Input.GetKey(KeyCode.Z))
+            {
+                animator.SetBool("Forward", true);
+                //animator.runtimeAnimatorController = animations.ToArray()[7];
+                if (rb.velocity.magnitude < limit)
+                    rb.AddForce(transform.forward * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                //animator.runtimeAnimatorController = animations.ToArray()[7];
+                if (rb.velocity.magnitude < limit)
+                    rb.AddForce(transform.forward * -speed);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                //animator.runtimeAnimatorController = animations.ToArray()[5];
+                if (rb.velocity.magnitude < limit)
+                    rb.AddForce(transform.right * -speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                //animator.runtimeAnimatorController = animations.ToArray()[5];
+                if (rb.velocity.magnitude < limit)
+                    rb.AddForce(transform.right * speed);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("Jump", true);
+                animator.SetBool("Grounded", false);
+                //animator.runtimeAnimatorController = animations.ToArray()[2];
+                rb.AddForce(transform.up * jump);
+
+            }
+
+            text.text = "Vitesse : " + rb.velocity.magnitude.ToString();
+            /*else
+            {
+                animator.runtimeAnimatorController = animations.ToArray()[1];
+            }*/
         }
-            
-        if (Input.GetKey(KeyCode.Z))
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[7];
-            if (rb.velocity.magnitude < 10f)
-                rb.AddForce(transform.forward * 6f) ;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[7];
-            if (rb.velocity.magnitude < 10f)
-                rb.AddForce(transform.forward * -6f);
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[5];
-            if (rb.velocity.magnitude < 10f)
-                rb.AddForce(transform.right * -6f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[5];
-            if (rb.velocity.magnitude < 10f)
-                rb.AddForce(transform.right * 6f);
-        }
-        if (Input.mousePosition != oldMousePosition)
-        {
-            float rotation = Input.mousePosition.x - oldMousePosition.x;
-            //rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -10, 0), 1);
-            rb.AddTorque(transform.up * rotation, ForceMode.VelocityChange);
-            oldMousePosition = Input.mousePosition;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[2];
-            rb.AddForce(transform.up * 20f);
-        }
-        /*else
-        {
-            animator.runtimeAnimatorController = animations.ToArray()[1];
-        }*/
     }
 }
