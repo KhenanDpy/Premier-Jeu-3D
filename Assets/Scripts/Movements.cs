e84e9cee5917d0862198325160317eb0a9420868
KhenanDpy <khdupuy@gmail.com>
2023-04-10 10:00:11 +0200

    controles maj
    bouger la camera par la souris,
    mieux bouger le personnage et pouvoir tourner

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,59 +1,80 @@
-using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Movements : MonoBehaviour
 {
-    [SerializeField]
-    CharacterController charCC;
+    Animator animator;
+    Rigidbody rb;
 
     [SerializeField]
-    Rigidbody cubeRb;
+    float rotate;
 
     [SerializeField]
-    float movementSpeed = 0.05f, gravity;
+    List<AnimatorController> animations = new List<AnimatorController>();
 
-    Vector3 movementDirection = Vector3.zero;
+    private Vector3 oldMousePosition;
 
-    [SerializeField]
-    Camera cameraGO; // permet de bouger le personnage par rapport à la camera
+    Vector3 m_EulerAngleVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        oldMousePosition = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementDirection.y -= gravity * Time.deltaTime;
-        Debug.Log(movementDirection.y);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+        if ((Input.anyKey == false) && (rb.position.y<=0.1f))
+        {
+            animator.runtimeAnimatorController = animations.ToArray()[1];
+            //rb.velocity = Vector3.zero;
+        }
+            
         if (Input.GetKey(KeyCode.Z))
         {
-            charCC.Move(cameraGO.transform.forward * movementSpeed);
-            cubeRb.AddForce(cameraGO.transform.forward * 6f, ForceMode.Force);
+            animator.runtimeAnimatorController = animations.ToArray()[7];
+            if (rb.velocity.magnitude < 10f)
+                rb.AddForce(transform.forward * 6f) ;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            charCC.Move(cameraGO.transform.forward * -movementSpeed);
-            cubeRb.AddForce(cameraGO.transform.forward * -6f, ForceMode.Force);
+            animator.runtimeAnimatorController = animations.ToArray()[7];
+            if (rb.velocity.magnitude < 10f)
+                rb.AddForce(transform.forward * -6f);
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            charCC.Move(cameraGO.transform.right * -movementSpeed);
-            cubeRb.AddForce(cameraGO.transform.right * -6f, ForceMode.Force);
+            animator.runtimeAnimatorController = animations.ToArray()[5];
+            if (rb.velocity.magnitude < 10f)
+                rb.AddForce(transform.right * -6f);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            charCC.Move(cameraGO.transform.right * movementSpeed);
-            cubeRb.AddForce(cameraGO.transform.right * 6f, ForceMode.Force);
+            animator.runtimeAnimatorController = animations.ToArray()[5];
+            if (rb.velocity.magnitude < 10f)
+                rb.AddForce(transform.right * 6f);
+        }
+        if (Input.mousePosition != oldMousePosition)
+        {
+            float rotation = Input.mousePosition.x - oldMousePosition.x;
+            //rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -10, 0), 1);
+            rb.AddTorque(transform.up * rotation, ForceMode.VelocityChange);
+            oldMousePosition = Input.mousePosition;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            charCC.Move(cameraGO.transform.up * movementSpeed * 50);
-            cubeRb.AddForce(cameraGO.transform.up * 300f, ForceMode.Force);
+            animator.runtimeAnimatorController = animations.ToArray()[2];
+            rb.AddForce(transform.up * 20f);
         }
+        /*else
+        {
+            animator.runtimeAnimatorController = animations.ToArray()[1];
+        }*/
     }
 }
